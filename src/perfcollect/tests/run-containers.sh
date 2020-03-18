6e4866ee1656d083034948e0f63e4923a655f2a0
Brian Robbins <brianrob@microsoft.com>
2020-03-18 13:26:43 -0700

    Enable testing for Ubuntu.

--- src/perfcollect/tests/run-containers.sh
+++ src/perfcollect/tests/run-containers.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
-echo "Run Containers"
+# Enable debug output to enable diagnostics in the CI.
+set -x
+
+# Run containers.
+docker run --privileged --security-opt seccomp=unconfined perfcollect-ubuntu-16.04
+docker run --privileged --security-opt seccomp=unconfined perfcollect-ubuntu-18.04
